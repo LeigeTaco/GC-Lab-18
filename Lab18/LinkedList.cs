b2cf385a7c326b4b32b7e7a595fe97c14735d339
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-08-09 10:24:48 -0400

    Frequency algorithim and printing working but could be better.

---
+++ Lab18/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Lab18
+//{
+
+//    public static class LinkedListExstensions
+//    {
+
+//        public static bool RemoveAt(this LinkedList<int> list, int index)
+//        {
+
+
+
+//            try
+//            {
+//                list.
+//            }
+//            catch (Exception)
+//            {
+//                return false;
+//            }
+
+//        }
+
+//        public static void PrintReverse(this LinkedList<int> list)
+//        {
+//            int x = list.Count;
+//            int i;
+//            for (i = 0; i < list.Count; i++)
+//            {
+//                list = list.Next
+//            }
+//            for (i = x - 1; i >= 1; i--)
+//            {
+//                Console.Write($"{list}, ");
+//            }
+//            Console.Write($"{list}\n");
+
+//        }
+
+//    }
+
+//}
--- Lab18/Program.cs
+++ Lab18/Program.cs
@@ -6,10 +6,65 @@ using System.Threading.Tasks;
 
 namespace Lab18
 {
+
     class Program
     {
+
+        static int[,] Frequency(int[] arr)
+        {
+
+            int min = arr[0];
+            int max = arr[0];
+            int count = 0;
+
+            foreach (int x in arr)
+            {
+                if (min > x)
+                {
+                    min = x;
+                }
+                if (max < x)
+                {
+                    max = x;
+                }
+            }
+            for (int i = 0; min + i <= max; i++)
+            {
+                if (min + i == max)
+                {
+                    count = i;
+                }
+            }
+            count++;
+            int[,] output = new int[count, 2];
+            for (int i = 0; i < count; i++)
+            {
+                output[i, 0] = min + i;
+                output[i, 1] = 0;
+            }
+            foreach (int x in arr)
+            {
+                int index = x - min;
+                output[index, 1]++;
+            }
+
+            return output;
+
+        }
+
         static void Main(string[] args)
         {
+
+            int[] a = {1,2,3,2,2,4,5,5,7,8,4,4,1,0,10 };
+            int[,] b = Frequency(a);
+            int length = b.GetLength(0);
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"{b[i,0]}: {b[i,1]}");
+            }
+
         }
+
     }
+
 }
